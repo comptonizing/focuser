43c8ddc439e321624c80d75e9aa3616bcb505dc7
Philipp Weber <philipp.ph.weber@fau.de>
2024-01-06 21:22:49 +0100

    Fix: Checksum calculation

--- firmware/firmware/Settings.cpp
+++ firmware/firmware/Settings.cpp
@@ -99,6 +99,13 @@ void Settings::sendMessage(char *msg) {
   crcMsg[0] = ((char *) &crc)[0];
   crcMsg[1] = ((char *) &crc)[1];
   crcMsg[2] = '\0';
+  // Must handle '$' which is end of message character
+  if ( crcMsg[0] == '$' ) {
+	  crcMsg[0] = '1';
+  }
+  if ( crcMsg[1] == '$' ) {
+	  crcMsg[1] = '1';
+  }
   Serial.print(MSG_PREFIX);
   Motor::i().update();
   Serial.print(msg);
@@ -120,6 +127,13 @@ void Settings::sendMessage(const __FlashStringHelper *msg) {
   crcMsg[0] = ((char *) &crc)[0];
   crcMsg[1] = ((char *) &crc)[1];
   crcMsg[2] = '\0';
+  // Must handle '$' which is end of message character
+  if ( crcMsg[0] == '$' ) {
+	  crcMsg[0] = '1';
+  }
+  if ( crcMsg[1] == '$' ) {
+	  crcMsg[1] = '1';
+  }
   Serial.print(MSG_PREFIX);
   Motor::i().update();
   Serial.print(msg);
